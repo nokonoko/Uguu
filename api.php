3ce12daea42db6c6573c759aae56d1ec44738a11
Eric Johansson (neku) <neku@pomf.se>
2015-02-07 19:28:24 +0100

    Update api.php

--- api.php
+++ api.php
@@ -9,12 +9,12 @@ if(isset($_GET['d'])) {
 	if(!empty($_POST['name'])){
 	$name = $_POST['name'];}
 	
+	if(!empty($_POST['autoext'])){
+	//
+	}
+	
 	save_file($_FILES['file']['tmp_name'], $name);
 	break;
-	
-        case 'upload-sharex':
-
-        break;
 
         case 'extend-time':
             break;
@@ -23,5 +23,5 @@ if(isset($_GET['d'])) {
 	break;
     }
 }else{
-    exit('Please provide a valid argument. Example: curl -i -F name=test.jpg -F file=@localfile.jpg http://uguu.se/api.php?d=upload');
+    header('Location: http://uguu.se');
 }
