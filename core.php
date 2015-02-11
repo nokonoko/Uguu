8c8107343a4f0ab8699c0cf4ce537358a43c476a
Eric Johansson (neku) <neku@pomf.se>
2015-02-11 03:31:27 +0100

    Update core.php

--- core.php
+++ core.php
@@ -1,16 +1,20 @@
 <?php
 function save_file ($file, $name){
-    //Generate a random set of numbers to set in front of the filename
-    $rand_string = crc32(microtime(true).mt_rand(1000, 9000));
+    //Generate a random prefix, strip tags and remove any whitespace
+    $file_name = gen_name(strip_tags(preg_replace('/\s+/', '', $name)));
     //Where to save
     $path='/home/neku/www/files/';
-    //Remove any tags
-    $file_data=strip_tags($file);
-    //Put together the random string+filename
-    $file_name=$rand_string.'_'.$name;
     //Move the file to the above location with said filename
-    move_uploaded_file($file_data,$path.$file_name);
+    move_uploaded_file($file,$path.$file_name);
     //Return url+filename to the user
     echo 'http://a.uguu.se/'.$file_name;
+}
+Function gen_name ($in){
+    $chars = 'abcdefghijklmnopqrstuvwxyz';
+    $name = '';
+    for ($i = 0; $i < 6; $i++) {
+    $name .= $chars[mt_rand(0, 25)];
+    }
+    return $name.'_'.$in;
 }
 ?>
