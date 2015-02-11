41b2390c28efd42a7a80dfec6bff137173e1cafe
Eric Johansson (neku) <neku@pomf.se>
2015-02-11 17:28:51 +0100

    Update core.php

--- core.php
+++ core.php
@@ -1,25 +1,42 @@
 <?php
-function save_file ($file, $name){
+function save_file ($file, $name, $arg){
     //Where to save
     $path='/home/neku/www/files/';
-    //Generate prefix, put together name and remove tags/whitespace
-    $file_name = strip_tags(preg_replace('/\s+/', '', $name));
-    $file_name = gen_name($file_name);
-    while(file_exists($path.$file_name)){
-        $file_name = gen_name(file_name);
+    //Generate name depending on arg
+    switch($arg){
+        case 'rand':
+            $ext = pathinfo($file, PATHINFO_EXTENSION);
+            $file_name gen_name('random', $ext);
+            while(file_exists($path.file_name)){
+                $file_name gen_name('random', $ext);
+            }
+            break;
+        case 'custom_original':
+            $name = strip_tags(preg_replace('/\s+/', '', $name));
+            $file_name = gen_name('custom_original', $name);
+            while(file_exists($path.$file_name)){
+                $file_name = gen_name('custom_original', $name);
+            }
+            break;
     }
     //Move the file to the above location with said filename
     move_uploaded_file($file,$path.$file_name);
     //Return url+filename to the user
     echo 'http://a.uguu.se/'.$file_name;
 }
-Function gen_name ($in){
-    //Generate random prefix
+function gen_name($arg, $in, $in2){
     $chars = 'abcdefghijklmnopqrstuvwxyz';
     $name = '';
     for ($i = 0; $i < 6; $i++) {
     $name .= $chars[mt_rand(0, 25)];
         }
-    return $name.'_'.$in;
+    switch($arg){
+        case 'random':
+            return $name.'.'.$in;
+            break;
+        case 'custom_original':
+            return $name.'_'.$in;
+            break;
+    }
 }
 ?>
