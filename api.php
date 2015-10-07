f6df8f516a38f901e99d639d07c1b6b706846260
Eric Johansson (neku) <neku@pomf.se>
2015-10-07 22:53:11 +0200

    Update api.php

--- api.php
+++ api.php
@@ -4,6 +4,7 @@ if(isset($_GET['d'])) {
     //Include the core file with the functions
     include_once('includes/core.php');
     switch ($_GET['d']) {
+    	//Uploading with HTML response and errors
         case 'upload':
         //If no file is being posted, show the error page and exit.
         if(empty($_FILES['file']['name'])){
@@ -21,13 +22,32 @@ if(isset($_GET['d'])) {
         $name = $_FILES['file']['name'];
 	$arg = 'random';}
 	//Call the save function which sends the file+name
-	save_file($_FILES['file']['tmp_name'], $name, $arg);
+	save_file($_FILES['file']['tmp_name'], $name, $arg, 'normal');
+	break;
+	//Uploading without HTML response or errors
+	case 'upload-tool':
+	        //If no file is being posted, show the error page and exit.
+        if(empty($_FILES['file']['name'])){
+		exit('You did not send a file, try again.');
+        }
+        //Set the name value to the original filename
+	$name = $_FILES['file']['name'];
+	$arg = 'custom_original';
+	//If the value name contains a custom name, set the name value
+	if(!empty($_POST['name'])){
+	$name = $_POST['name'];}
+	//If value contains anything, keep original filename
+	if(!empty($_POST['randomname'])){
+        $name = $_FILES['file']['name'];
+	$arg = 'random';}
+	//Call the save function which sends the file+name
+	save_file($_FILES['file']['tmp_name'], $name, $arg, 'tool');
 	break;
         case 'extend-time':
             break;
 	default:
 	//If no correct valid argument for the api to perform on, tell them to enter a valid one
-	exit('Please provide a valid argument. Example: curl -i -F name=test.jpg -F file=@localfile.jpg http://uguu.se/api.php?d=upload');
+	exit('Please provide a valid argument. Example: curl -i -F name=test.jpg -F file=@localfile.jpg http://uguu.se/api.php?d=upload-tool');
 	break;
     }
 }else{
