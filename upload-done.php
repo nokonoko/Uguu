e76971212dee09dc5d0279243497c0c1635af367
Henrik <henrik@neef-privat.de>
2018-08-09 08:10:59 +0200

    fix upload path

--- upload-done.php
+++ upload-done.php
@@ -10,7 +10,7 @@ $tpl->draw("header");
 if(CONFIG_SUBUPLOAD_URL_ENABLED == 'true'){
   $tpl->assign("url_filename", CONFIG_SUBUPLOAD_URL.'/'.$n);
 }else{
-  $tpl->assign("url_filename", CONFIG_ROOT_URL.'/files/'.$n);
+  $tpl->assign("url_filename", CONFIG_ROOT_URL.'/public/files/'.$n);
 }
 $tpl->assign("retention_time", CONFIG_MAX_RETENTION_TEXT);
 $tpl->draw("upload-done");
