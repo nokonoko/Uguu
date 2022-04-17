b3c0237ad9cc9d83cc1cb7ff66a8e2fd9d299515
nokonoko <neku@pomf.se>
2022-04-17 15:14:43 +0200

    docker support added
    using alpine for the docker instead of ubuntu cuts the size and installation time by quite a bit

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-envsubst < /var/www/uguu/_dist.json > /var/www/uguu/dist.json 
 cd /var/www/uguu/
 make
 make install
-service php8.0-fpm start
-service cron start
-nginx -g 'daemon off;'
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
---
+++ docker_purge.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+make DOCKER_TAG=$(cat package.json | grep version | cut -d '"' -f4) CONTAINER_NAME=uguu purge-container
