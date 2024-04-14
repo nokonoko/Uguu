c06ab66eda766f5ab225463a71d6737cbc3f0d87
Go Johansson (neku) <neku@pomf.se>
2024-04-14 17:13:01 +0200

    Update runBenchmark.sh

--- src/Benchmarks/runBenchmark.sh
+++ src/Benchmarks/runBenchmark.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 cd ..
-sqlite3 /Users/neku/repos/Uguu/dist/Benchmarks/tmp/uguu.sq3 -init /Users/neku/repos/Uguu/src/static/dbSchemas/sqlite_schema.sql ""
 ./vendor/bin/phpbench run Benchmarks --report=default
-rm -rf Benchmarks/tmp/*.jpg
-rm -rf Benchmarks/tmp/uguu.sq3
